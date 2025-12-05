07a3b8a37c3a861a4a61dddb54d3c6a04ae68c51
A.Lienhard.inf23 <A.Lienhard.inf23@stud.bbbaden.ch>
2025-12-05 13:17:44 +0100

    Fixed issue with drawing the stations. Added a label to the stations, so the name gets displayed. Started working on a tooltip that appears when hovering over the stationn.

--- script.js
+++ script.js
@@ -4,38 +4,74 @@ const height = 600;
 
 const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
 
-d3.json('./assets/data/stations.json')
-  .then(data => {
-    console.log('Loaded stations!');
-    console.log(data);
-    
-    const stations = Object.values(data); // converts object → array
-
-    const factionColors = {
-      "Hansa" : "brown",
-      "Red Line": "red",
-      "Fourth Reich": "gray",
-      "Neutral": "yellow"
-    };
-
-
-    svg.selectAll("circle.station")
-      .data(stations)
-      .enter()
-      .append("circle")
-      .attr("class", "station")
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y)
-      .attr("r", 6)
-      .style("fill", d => factionColors[d.faction] || "black")
-      .style("stroke", "black");
-  });
 
  Promise.all([
     d3.json('./assets/data/stations.json'),
     d3.json('./assets/data/tunnels.json')
  ]).then(([stationsData, tunnelsData]) => {
-    const stations = Object.entries(stationsData).map(([id, obj]) => {
-        return {id, obj};
-    })
- });
+   
+    //Check if everything loaded correctly
+  console.log("Loaded stations and tunnels!");
+  console.log(stationsData, tunnelsData);
+    //Code for drawing tunnels
+    svg.selectAll("line.tunnel")
+    .data(tunnelsData)
+    .enter()
+    .append("line")
+    .attr("class", "tunnel")
+    .attr("x1", d => stationsData[d.from].x)
+    .attr("y1", d => stationsData[d.from].y)
+    .attr("x2", d => stationsData[d.to].x)
+    .attr("y2", d => stationsData[d.to].y)
+    .style("stroke", "black")
+    .style("stroke-width", 5)
+    .style("opacity", 0.6);
+    
+
+
+     const factionColors = {
+      "Hansa" : "brown",
+      "Red Line": "red",
+      "Fourth Reich": "gray",
+      "VDNKh Commonwealth": "blue",
+      "Neutral": "yellow" };
+    
+      //Code for drawing stations
+      var stationsArray = Object.values(stationsData); // Object -> Array, because D3 converted JSON -> Object already
+
+      const stationGroups = svg.selectAll("g.station")
+      .data(stationsArray)
+      .enter()
+      .append("g")
+      .attr("class", "station")
+      .attr("transform", d => `translate(${d.x}, ${d.y})`);
+
+      stationGroups.append("circle")
+      .attr("r", 15)
+      .style("fill", d => factionColors[d.faction] || "black")
+      .style("stroke", "black")
+      .style("stroke-width", 1.7)
+      .append("title")
+      .text(d => d.name);
+
+      // stationGroups.append("title")
+      // .text(d => d.name);
+
+      stationGroups.append("text")
+      .text(d => d.name)
+      .attr("y", -20)
+      .attr("text-anchor", "middle")
+      .style("font-size", "12px")
+      .style("fill", "black")
+       .style("pointer-events", "none");
+
+
+      // Tooltip reference for self: https://stackoverflow.com/questions/10805184/show-data-on-mouseover-of-circle
+//       const tooltip = d3.selectAll("circle")
+//       .data(stationsArray)
+//       .enter()
+//       .append("svg:circle")
+//       .append("svg:title")
+//       .text(function(d) {return d.x});
+  
+  });
